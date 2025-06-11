ba1418d685032bd2c5956b0cdd722cf60ff66499
Mayyada Elsayed <mayydaelsayed8@gmail.com>
2025-06-11 21:44:38 +0300

    Save user_id, user_email in data store.

---
+++ app/src/main/java/com/example/vendora/di/AppModule.kt
@@ -0,0 +1,21 @@
+package com.example.vendora.di
+
+import android.content.Context
+import com.example.vendora.data.local.UserPreferences
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideUserPreferences(@ApplicationContext context: Context): UserPreferences {
+        return UserPreferences(context)
+    }
+}
