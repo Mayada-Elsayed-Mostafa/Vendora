a38d1992976c9d5d564fe6db66762db437bf5de8
Mayyada Elsayed <mayydaelsayed8@gmail.com>
2025-06-21 08:57:22 +0300

    provide FavoritesRepository implementation

--- app/src/main/java/com/example/vendora/di/AppModule.kt
+++ app/src/main/java/com/example/vendora/di/AppModule.kt
@@ -2,11 +2,15 @@ package com.example.vendora.di
 
 import android.content.Context
 import com.example.vendora.data.local.UserPreferences
+import com.example.vendora.data.repo_implementation.FavoritesRepositoryImpl
+import com.example.vendora.domain.repo_interfaces.FavoritesRepository
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
@@ -18,4 +22,17 @@ object AppModule {
     fun provideUserPreferences(@ApplicationContext context: Context): UserPreferences {
         return UserPreferences(context)
     }
+
+    @Provides
+    fun provideFirestore(): FirebaseFirestore = FirebaseFirestore.getInstance()
+
+    @Provides
+    fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
+
+    @Provides
+    fun provideFavoritesRepository(
+        firestore: FirebaseFirestore,
+        auth: FirebaseAuth
+    ): FavoritesRepository = FavoritesRepositoryImpl(firestore, auth)
+
 }
